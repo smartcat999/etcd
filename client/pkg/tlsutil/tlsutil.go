13ea4dfc0b4b753718b2e8268284d074264a9925
peng wu <2030047311@qq.com>
2023-10-12 09:11:41 +0800

    feat: support tls cert encrypt

--- client/pkg/tlsutil/tlsutil.go
+++ client/pkg/tlsutil/tlsutil.go
@@ -15,10 +15,13 @@
 package tlsutil
 
 import (
+	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"io/ioutil"
+	"os"
 )
 
 // NewCertPool creates x509 certPool with provided CA files.
@@ -51,6 +54,8 @@ func NewCertPool(CAFiles []string) (*x509.CertPool, error) {
 
 // NewCert generates TLS cert by using the given cert,key and parse function.
 func NewCert(certfile, keyfile string, parseFunc func([]byte, []byte) (tls.Certificate, error)) (*tls.Certificate, error) {
+	encKey := os.Getenv("KEY_PASS")
+
 	cert, err := ioutil.ReadFile(certfile)
 	if err != nil {
 		return nil, err
@@ -59,6 +64,12 @@ func NewCert(certfile, keyfile string, parseFunc func([]byte, []byte) (tls.Certi
 	key, err := ioutil.ReadFile(keyfile)
 	if err != nil {
 		return nil, err
+	}
+
+	if encKey != "" {
+		if pkey, err := ParseRSAPrivateKeyFromPEMWithPassword(key, encKey); err == nil {
+			key = ParseRSAPrivateKeyToMemory(pkey)
+		}
 	}
 
 	if parseFunc == nil {
@@ -70,4 +81,44 @@ func NewCert(certfile, keyfile string, parseFunc func([]byte, []byte) (tls.Certi
 		return nil, err
 	}
 	return &tlsCert, nil
+}
+
+func ParseRSAPrivateKeyFromPEMWithPassword(key []byte, password string) (*rsa.PrivateKey, error) {
+	var err error
+
+	// Parse PEM block
+	var block *pem.Block
+	if block, _ = pem.Decode(key); block == nil {
+		return nil, errors.New("invalid Key: Key must be PEM encoded PKCS1 or PKCS8 private key")
+	}
+
+	var parsedKey interface{}
+
+	var blockDecrypted []byte
+	if blockDecrypted, err = x509.DecryptPEMBlock(block, []byte(password)); err != nil {
+		return nil, err
+	}
+
+	if parsedKey, err = x509.ParsePKCS1PrivateKey(blockDecrypted); err != nil {
+		if parsedKey, err = x509.ParsePKCS8PrivateKey(blockDecrypted); err != nil {
+			return nil, err
+		}
+	}
+
+	var pkey *rsa.PrivateKey
+	var ok bool
+	if pkey, ok = parsedKey.(*rsa.PrivateKey); !ok {
+		return nil, errors.New("key is not a valid RSA private key")
+	}
+
+	return pkey, nil
+}
+
+func ParseRSAPrivateKeyToMemory(key *rsa.PrivateKey) []byte {
+	keybytes := x509.MarshalPKCS1PrivateKey(key)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: keybytes,
+	}
+	return pem.EncodeToMemory(block)
 }
---
+++ client/pkg/tlsutil/tlsutil_test.go
@@ -0,0 +1,52 @@
+package tlsutil
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"testing"
+)
+
+func TestAES256CBCExample01(t *testing.T) {
+	encryKey := `-----BEGIN RSA PRIVATE KEY-----
+Proc-Type: 4,ENCRYPTED
+DEK-Info: AES-256-CBC,BA9C8341E29725A4FAC0A365C298D653
+
+3Xw8R+JsAVQhEb57EpZUd7Wa/QZ1OqCGl++ZIZ7PbG9wteIn6C0L3+Yo1pZDMnCN
+dKuhndiYlLmIfrYmZtye5r9f2IYYizseAmFNag+f5e7kalUAqQUTT/soUBHEg5uu
+5u0W1NAioxZfnmrdd19+0lCp4Qbw1eV2Grh18NUJ2kWI4pK9QtxrgEdh1Zbr8u10
+ijqf/C+Xg3OZi/53zw147IRJbTSSUYdYCbE7V8Ii4EgmRD9QTaeTTn8HT07agaeR
+NfnWTYL5yTB551t4Ofogfzq8m3rlB1K1eQhVHdXxdZxl06iaK4CStiLAvrXBA5j9
+GOmGLZCtddoeJi7gUsO6Oqc4CIVhgXxejm3i8ACc4zPO7U/S1q+Ck64gVV2kFEK/
+1NdBdlpSy4IsThr5tGMNr1yYeHHbK99uqtKIUwHLckUYQtVu9cScx9ABX+aNZv2D
+O2BVAd/asEdezS9GSPNtmPQoRNGqnpBpOMT0TikGSeSsbobdMUVIcbqVV0A8ogzi
+WgPtUtwCPgLbYuJauqzwOVS9ErohefVHBV4+p4/Oa140fc8iEqLilVZgGLBZJJGB
+PgXmit+LKJzuzE6oqpiMs9T4BDWqFEHTcTNbdOezzDOCrYPFn7IinOLU4HPm7Tmn
+B4fP2YaR35bJKWYvjwxTwOcMTunl3BFxguoCpy6OPMgxWc875VKLGXJc4zOuiX6o
+nhq+26iQ2mIWylYZcj00H7ial/czoKTTZuGT0uK2vq97OHnKBt5GH1nYqOoHofIw
+BhR1OJWffzpTBzTjaLNOtIU2XTAZmeJlV7UB0Crgs7zUVLnofNhAmgkMGKZxKGfJ
+S4h8FVz13mXQw5O4hODRKIbDbcA2Cy4MUOhgCSR5sumfsKeJ/o4B9Dyzhguxl8Sb
+JosLFDsSF7f870TUO/oldngBhZqPVBuAD+tNl4VTcfqv1eDaQglb6wJ61TXiGIeV
+Mm+doMX160BO/e5wdKMq6dOzREEy+QKm8LA1JqVKqb0N0Tollyf0/Lo5p/SXbqeF
+5Kvyd/AxXTkSsKEt0eChZXX3SNzydKbSMqNsqzkST/1YiIulXtenhZrrNMdGnoJP
+EaGd+ANdQ28y97E+OoIVP/8qykuwTPSpsu/2TXxoESpHuBRZOzzgGTFBaUXyXbT4
+FJXTI0jghY8or5JkNW7JGAOMuQ8jQHnJNdyPFEBVbCjE4rZ+cuBBxCfracxehmw8
+Q0bBdrFzVWye+0b1fJP0jtMQVvmbReOt9EDJ8IZPRXuFFnON4pnJI89VAPXXemgX
+o+W90kIA3lFNJNKRHA5A82jYfxfdHezh/jVSuW3PnQu3SbpHhdpgJRokTVLFLx+Q
+aPzvWZnH8LtH63XLdZUsHqZvTEjH5hA4/sptQPlaeh3lYbDujcasRDAglQouXU33
+ZEbFbSjahO28ZE6NZeGZsRqoiHeJ6CmgMxabHlJOMtf76aUGVezo8VfLOHSQ01Tn
+q42BURh9TNHChMQOIpwB/MOAe6UYKRdbTGXR1Db/6HieDn8In7pTNfY0tLdFm6gS
+VvUbhwjHvQ8wI1dvd5+BH+i2S+4dhAm1jyyxOyj6XXd9WC8SsvcUKDpRfYWNb2at
+-----END RSA PRIVATE KEY-----`
+	key, err := ParseRSAPrivateKeyFromPEMWithPassword([]byte(encryKey), "test")
+	if err != nil {
+		t.Logf("ParseRSAPrivateKeyFromPEMWithPassword Err: %v", err)
+	}
+	t.Logf("origin data: %v", key)
+	keybytes := x509.MarshalPKCS1PrivateKey(key)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: keybytes,
+	}
+	privateKey := pem.EncodeToMemory(block)
+	t.Logf("MarshalPKCS1PrivateKey: %v", string(privateKey))
+}
